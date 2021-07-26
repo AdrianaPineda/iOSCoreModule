0ef92abf97f55738bf4b83a3646b09826133af6e
apineda-truora <apineda@truora.com>
2021-07-26 12:12:56 -0400

    add common to core

---
+++ build/iOSCoreModule.build/Debug-iphoneos/iOSCommonModule.build/Objects-normal/arm64/iOSCommonModule_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphoneos/iOSCommonModule.build/DerivedSources/iOSCommonModule_vers.c
---
+++ build/iOSCoreModule.build/Debug-iphonesimulator/iOSCommonModule.build/Objects-normal/x86_64/iOSCommonModule_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphonesimulator/iOSCommonModule.build/DerivedSources/iOSCommonModule_vers.c
