d1a497f09c16e0e57b7459cb247954a6d85f4ac8
apineda-truora <apineda@truora.com>
2021-07-26 12:01:26 -0400

    Sucess doc and core

--- build/iOSCoreModule.build/Debug-iphonesimulator/iOSCoreModule.build/Objects-normal/i386/iOSCoreModule_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphonesimulator/iOSCoreModule.build/DerivedSources/iOSCoreModule_vers.c
--- build/iOSCoreModule.build/Debug-iphonesimulator/iOSDocModule.build/Objects-normal/i386/iOSDocModule_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphonesimulator/iOSDocModule.build/DerivedSources/iOSDocModule_vers.c
