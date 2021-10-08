210e5bdd8e055b230eddce54b9918fed3f5a9af0
apineda-truora <apineda@truora.com>
2021-10-08 18:16:52 -0400

    use carthage for dependencies

--- build/iOSCoreModule.build/Debug-iphoneos/iOSCoreOnlyModule.build/Objects-normal/arm64/OptionalDepBridge.d
+++ build/iOSCoreModule.build/Debug-iphoneos/iOSCoreOnlyModule.build/Objects-normal/arm64/OptionalDepBridge.d
@@ -2,8 +2,8 @@ dependencies: \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/iOSCoreOnlyModule/OptionalDepBridge.m \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphoneos/iOSCoreOnlyModule.build/module.modulemap \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/iOSCoreOnlyModule/OptionalDepBridge.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/apineda/Downloads/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Users/apineda/Downloads/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphoneos/iOSCommonModule.build/module.modulemap \
   /Users/apineda/Library/Developer/Xcode/DerivedData/iOSCoreModule-glxxzloisdxojyhikjjfsgyfumig/Build/Products/Debug-iphoneos/iOSCommonModule.framework/Headers/iOSCommonModule-Swift.h \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/iOSCommonModule/iOSCommonModule.h \
--- build/iOSCoreModule.build/Debug-iphonesimulator/iOSCoreOnlyModule.build/Objects-normal/x86_64/OptionalDepBridge.d
+++ build/iOSCoreModule.build/Debug-iphonesimulator/iOSCoreOnlyModule.build/Objects-normal/x86_64/OptionalDepBridge.d
@@ -2,8 +2,8 @@ dependencies: \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/iOSCoreOnlyModule/OptionalDepBridge.m \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphonesimulator/iOSCoreOnlyModule.build/module.modulemap \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/iOSCoreOnlyModule/OptionalDepBridge.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/apineda/Downloads/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Users/apineda/Downloads/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/build/iOSCoreModule.build/Debug-iphonesimulator/iOSCommonModule.build/module.modulemap \
   /Users/apineda/Library/Developer/Xcode/DerivedData/iOSCoreModule-glxxzloisdxojyhikjjfsgyfumig/Build/Products/Debug-iphonesimulator/iOSCommonModule.framework/Headers/iOSCommonModule-Swift.h \
   /Users/apineda/Documents/truora/samples/modularized-ios/iOSCoreModule/iOSCommonModule/iOSCommonModule.h \
